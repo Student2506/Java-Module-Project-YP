28bcff75fa0ffc513782d1d7954cc7350eb680d2
Захаров Павел <pzakharov83@gmail.com>
2023-11-23 18:28:10 +0300

    add special case with 11..19 roubles

--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -1,5 +1,8 @@
 public class Formatter {
     public static String conjugate(int count) {
+        if (11 <= count && count <= 19) {
+            return "рублей";
+        }
         return switch (count % 10) {
             case 1 -> "рубль";
             case 2, 3, 4 -> "рубля";
